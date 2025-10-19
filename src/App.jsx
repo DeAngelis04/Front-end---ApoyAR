5d2c0b086094a9f7b196018abf28b1c0ecdb2ae6
Juan Ignacio De Angelis <juandeangelis2004gmail.com>
2025-10-19 12:12:06 -0300

    Agregar archivos faltantes

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,20 @@ import Footer from "./components/Footer/Footer";
 import Formulario from "./components/Formulario/Formulario";
 import Nav from "./components/Nav/Nav";
 import Darkmode from "./components/Nav/Tema";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
       <Nav />
-      
-        <Formulario />
-      <Footer/>
+      <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="Inicio" element={<Inico/>} />
+        </Routes>
+      </div>
+      </BrowserRouter>
+      <Formulario />
+      <Footer />
     </>
   );
 }
